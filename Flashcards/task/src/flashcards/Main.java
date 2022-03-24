00f99a77bc20afe0c0b7fa3c34b27f6c9d2a12cb
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-24 15:30:36 +0200

    What’s on the card

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,11 +1,17 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                Card:
-                purchase
-                Definition:
-                buy""");
+        Scanner scanner = new Scanner(System.in);
+        String term = scanner.nextLine();
+        String definition = scanner.nextLine();
+        String userInput = scanner.nextLine();
+        if (definition.equals(userInput)) {
+            System.out.println("right");
+        } else {
+            System.out.println("wrong");
+        }
     }
 }
