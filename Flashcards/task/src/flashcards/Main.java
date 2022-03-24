c446fe8ea33ff573dd2e53811ec3110a1c476ca4
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-24 17:26:19 +0200

    Make it your own

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,17 +1,32 @@
 package flashcards;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Input the number of cards:");
         Scanner scanner = new Scanner(System.in);
-        String term = scanner.nextLine();
-        String definition = scanner.nextLine();
-        String userInput = scanner.nextLine();
-        if (definition.equals(userInput)) {
-            System.out.println("right");
-        } else {
-            System.out.println("wrong");
+        int cards = Integer.parseInt(scanner.nextLine());
+        ArrayList<String> terms = new ArrayList<>();
+        ArrayList<String> definitions = new ArrayList<>();
+        for (int i = 1; i <= cards; i++) {
+            System.out.println("Card #" + i + ":");
+            terms.add(scanner.nextLine());
+            System.out.println("The definition for card #" + i + ":");
+            definitions.add(scanner.nextLine());
+        }
+
+        for (int i = 0; i < cards; i++) {
+            System.out.println("Print the definition of \"" + terms.get(i) + "\"");
+            String userInput = scanner.nextLine();
+            if (userInput.equals(definitions.get(i))) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Wrong. The right answer is \"" + definitions.get(i) + "\".");
+            }
         }
     }
 }
