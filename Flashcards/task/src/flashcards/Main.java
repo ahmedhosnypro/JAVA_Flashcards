61ba62ca0447e7095bf6a031d40701758e7af848
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-24 15:22:27 +0200

    Stage one, card one

---
+++ Flashcards/task/src/flashcards/Main.java
@@ -0,0 +1,11 @@
+package flashcards;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Card:
+                purchase
+                Definition:
+                buy""");
+    }
+}
